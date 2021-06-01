498050dbcffc989928b84665be6d622c39afed5d
Tao He <linzhu.ht@alibaba-inc.com>
2021-06-01 17:50:31 +0800

    Update for helm script.
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- charts/hack/install.sh
+++ charts/hack/install.sh
@@ -6,7 +6,8 @@ set -o pipefail
 
 ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
-helm install vineyard $ROOT/vineyard/ -n vineyard-system --set vineyard.sharedMemorySize=8Gi
+helm install vineyard $ROOT/vineyard/ -n vineyard-system \
+    --set vineyard.sharedMemorySize=8Gi
 
 set +x
 set +e
